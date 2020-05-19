ac421c916b72ca537a523407a3386af620ddc1e0
Jose Marquez <josesinmarquezpalmeros@gmail.com>
2020-05-18 21:07:10 -0500

    initial components complete

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './App.scss';
 
+import Auth from '../components/Auth/Auth';
+import MyNavbar from '../components/MyNavbar/MyNavbar';
+import Team from '../components/Team/Team';
+
 class App extends React.Component {
   render() {
     return (
       <div className="App">
+        <MyNavbar/>
         <h2>Testing</h2>
+        <Auth/>
+        <Team/>
         <button className="btn btn-info"><i class="fas fa-rocket"></i></button>
       </div>
     );
---
+++ src/components/Team/Team.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Team.scss';
+
+class Team extends React.Component {
+  render() {
+    return (
+      <div className="Team">
+        <h1>Inside Team</h1>
+      </div>
+    );
+  }
+}
+
+export default Team;
